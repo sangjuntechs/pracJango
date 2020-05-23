aa5d5764abd1c6d4a49205b3bdb6fe499835b117
sangjun <juntech0421@github.com>
2020-05-23 14:36:44 +0900

    admin prac

--- community/user/admin.py
+++ community/user/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import User
+class userAdmin(admin.ModelAdmin):
+    list_display = ('username','password','regiester_dttm')
 
-# Register your models here.
+admin.site.register(User, userAdmin)
+
--- community/user/models.py
+++ community/user/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class User (models.Model):
+    username = models.CharField(max_length=32, verbose_name='사용자명')
+
+    password = models.CharField(max_length=64, verbose_name='비밀번호')
+
+    regiester_dttm = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
+
+    def __str__(self):
+        return self.username
+
+    class Meta:
+        db_table = 'user_info'
+        verbose_name = '사용자'
+        verbose_name_plural = '사용자'
