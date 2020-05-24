abbcb99c50d91c796e1e40fbe15d022edd8b8a31
sangjun <juntech0421@github.com>
2020-05-24 16:03:13 +0900

    add code view.py

--- community/user/views.py
+++ community/user/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+from .models import User
 
 def register(request) :
     if request.method == 'GET':
         return render(request,'register.html')
     elif request.method == 'POST':
-        return render(request,'register.html')
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+
+        user = User(
+            username = username,
+            password = password
+        )
+
+        user.save()
+
+        return render(request, 'register.html')
         
