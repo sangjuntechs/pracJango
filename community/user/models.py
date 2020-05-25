9751dd31755567f0c7687719ae890218275c5126
sangjun <juntech0421@github.com>
2020-05-25 23:35:50 +0900

    add login code

--- community/user/models.py
+++ community/user/models.py
@@ -1,12 +1,10 @@
 from django.db import models
 
 class User (models.Model):
+    objects = models.Manager()
     username = models.CharField(max_length=32, verbose_name='사용자명')
-
     password = models.CharField(max_length=64, verbose_name='비밀번호')
-
     regiester_dttm = models.DateTimeField(auto_now_add=True, verbose_name='등록시간')
-
     useremail = models.EmailField(max_length=128, verbose_name='이메일')
 
     def __str__(self):
--- community/user/views.py
+++ community/user/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
-from .models import User
 from django.http import HttpResponse
-from django.contrib.auth.hashers import make_password
+from django.contrib.auth.hashers import make_password, check_password
+from .models import User
+
+def login(request):
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    elif request.method =='POST':
+        username = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+        
+        res_data={}
+        if not (username and password):
+            res_data['error'] = '올바른 정보를 입력하세요.'
+        else:
+            user = User.objects.get(username=username)
+            if check_password(password, user.password):
+                res_data['error'] = '로그인 성공'
+            else:
+                res_data['error'] = '비밀번호가 틀렸습니다.'
+    return render(request, 'login.html',res_data)
+
 
 def register(request) :
     if request.method == 'GET':
